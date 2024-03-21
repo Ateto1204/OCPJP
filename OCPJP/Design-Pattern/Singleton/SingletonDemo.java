3f1465c42898f92fea7c23a6cc3e56497b032313
Ateto1202 <tonysu1204@gmail.com>
2024-03-21 21:00:11 +0800

    Sort out repo

---
+++ OCPJP/Design-Pattern/Singleton/SingletonDemo.java
@@ -0,0 +1,35 @@
+/*
+ * 
+ * Author: Ateto
+ * 
+ * Description: Demo to Singleton design pattern
+ * 
+ */
+
+package build;
+
+class SingletonClass {
+
+    int data = 10;
+
+    private SingletonClass() {
+
+    }
+
+    private static final SingletonClass instance = new SingletonClass();
+
+    public static SingletonClass getInstance() {
+        return instance;
+    }
+}
+
+public class SingletonDemo {
+    public static void main(String[] args) {
+        SingletonClass ref1 = SingletonClass.getInstance();
+        SingletonClass ref2 = SingletonClass.getInstance();
+
+        ref1.data = 5;
+
+        System.out.println(ref2.data);
+    }
+}
